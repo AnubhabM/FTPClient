30cf5a5f504072d86daa4a197ff5b734efc48721
Anubhab Mondal <Anubhab.Mondal@smarsh.com>
2019-11-04 13:17:22 +0530

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InitAppComponent } from './init-app/init-app.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {path: 'login' , component: InitAppComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InitAppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes , {useHash: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
